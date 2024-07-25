e3bb26c1ddd64fc2ea267f62fd02d8e7be9bbe84
Junyoung Kim <145259647+adidq@users.noreply.github.com>
2024-07-25 20:55:35 +0900

    Update nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -17,9 +17,6 @@ export default {
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
-    new WebpackObfuscator ({
-        rotateStringArray: true
-    }, ['excluded_bundle_name.js'])
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
